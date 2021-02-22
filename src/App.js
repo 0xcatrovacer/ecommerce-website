b692611218a8cae57b6575c62b2b7545657ad7b9
Swarnab Garang <swarnabgarang@gmail.com>
2021-02-22 18:20:08 +0530

    added home container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* Navbar */}
       <Navbar />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="HomeContainer">
+                <img className="HomeImage" src="./banner3.jpg" alt="" />
+
+                <div className="HomeRow">
+                
+                
+                </div>
+                <div className="HomeRow">
+                
+                
+                </div>
+                <div className="HomeRow">
+                
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
