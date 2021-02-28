82d8164f9ad9e53e849bbd2110ac150f314e2b49
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-01 01:49:23 +0530

    added transition design to Product buttons

--- src/components/Home/Product/Product.js
+++ src/components/Home/Product/Product.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStateValue } from '../../../StateProvider'
 import './Product.css'
 
-const Product = ( { title, image, price, rating } ) => {
+const Product = ({ title, image, price, rating }) => {
     const [{ cart }, dispatch] = useStateValue();
 
     const addToCart = () => {
@@ -22,17 +22,17 @@ const Product = ( { title, image, price, rating } ) => {
             <div className="ProductInfo">
                 <p>{title}</p>
                 <p className="ProductPrice">
-                <strong>₹</strong>
-                <strong> {price}</strong>
+                    <strong>₹</strong>
+                    <strong> {price}</strong>
                 </p>
-                <div className="ProductRating"> 
+                <div className="ProductRating">
                     {Array(rating).fill().map((_, i) => (
                         <span>&#11088;</span>
                     ))}
                 </div>
             </div>
             <img src={image} alt="" />
-            
+
             <button onClick={addToCart}>Add to Cart</button>
         </div>
     )
